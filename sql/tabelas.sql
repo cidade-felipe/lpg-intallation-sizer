f4ef43a5ca4426a02d296f8799a228e311e89613
felipecidade94 <felipecidade94@gmail.com>
2026-01-27 09:24:27 -0300

    tabela peca e sem tabelas conexao nem acessorios, pois arrumei o json de pecas

--- sql/tabelas.sql
+++ sql/tabelas.sql
@@ -60,23 +60,15 @@ CREATE TABLE IF NOT EXISTS tubo(
    FOREIGN KEY (material_id) REFERENCES material(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS conexao(
+CREATE TABLE IF NOT EXISTS peca(
    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
    material_id INTEGER NOT NULL,
+   categoria VARCHAR(50) NOT NULL CHECk(categoria IN ('Conexões', 'Acessórios')),
    diametro VARCHAR(10) NOT NULL,
    nome VARCHAR(50) NOT NULL,
    comprimento_equivalente REAL NOT NULL,   
-   UNIQUE(material_id, diametro,nome),
+   UNIQUE(material_id, categoria, diametro, nome),
    FOREIGN KEY (material_id) REFERENCES material(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS acessorio(
-   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   material VARCHAR(100) NOT NULL CHECK(material IN ('Metal','PEX')),
-   diametro VARCHAR(10) NOT NULL,
-   nome VARCHAR(50) NOT NULL,   
-   comprimento_equivalente REAL NOT NULL,
-   UNIQUE(material, diametro, nome)
 );
 
 CREATE TABLE IF NOT EXISTS trecho(
@@ -90,14 +82,11 @@ CREATE TABLE IF NOT EXISTS trecho(
    perda_carga REAL
 );
 
-CREATE TABLE IF NOT EXISTS trecho_conexao_acessorio(
+CREATE TABLE IF NOT EXISTS trecho_peca(
    trecho_id INTEGER NOT NULL,
-   conexao_id INTEGER NOT NULL,
-   acessorio_id INTEGER NOT NULL,
+   peca_id INTEGER NOT NULL,
    quantidade_peca INTEGER NOT NULL,
-   quantidade_acessorio INTEGER NOT NULL,
-   PRIMARY KEY(trecho_id, conexao_id, acessorio_id),
+   PRIMARY KEY(trecho_id, peca_id),
    FOREIGN KEY (trecho_id) REFERENCES trecho(id) ON DELETE CASCADE,
-   FOREIGN KEY (conexao_id) REFERENCES conexao(id) ON DELETE CASCADE,
-   FOREIGN KEY (acessorio_id) REFERENCES acessorio(id) ON DELETE CASCADE
+   FOREIGN KEY (peca_id) REFERENCES peca(id) ON DELETE CASCADE
 );
